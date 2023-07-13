c08b6c0788877e7391691c9ac6fbdad1e23e3aa0
Pedro Plasencia <programacion.en.esp@gmail.com>
2023-07-13 23:45:54 +0200

    feat: initial commit with angular app and github actions script

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path:'home', loadChildren:() => import('./pages/home/home.module').then(m => m.HomeModule)},
+  {path:'about', loadChildren:() => import('./pages/about/about.module').then(m => m.AboutModule)},
+  {path:'', redirectTo:'/home', pathMatch:'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
